e2a8707c4ba6a44b65831e809382de5a29afcb37
lukaszmar <lukaszmar@gmail.com>
2023-10-30 09:36:02 +0100

    add polishking

---
+++ filelm/PolishKing.js
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+
+const PolishKing = ()=>{
+
+    return(
+        <View>
+            <Text>polish King is comming</Text>
+        </View>
+    )
+}
+
+export default PolishKing;
